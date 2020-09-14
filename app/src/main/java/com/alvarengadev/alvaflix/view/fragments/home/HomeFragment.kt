ac00ebdfc50b5f4f2ec9bc9d188dcc8f025eb3e6
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-09-14 18:37:17 -0300

    chore: add HomeFragment

---
+++ app/src/main/java/com/alvarengadev/alvaflix/view/fragments/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.alvarengadev.alvaflix.view.fragments.home
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alvarengadev.alvaflix.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/alvarengadev/alvaflix/view/fragments/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.alvarengadev.alvaflix.view.fragments.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
