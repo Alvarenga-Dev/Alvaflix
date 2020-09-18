c3909f14270137b280070b6dfa98395ca660b5e9
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-09-18 11:14:56 -0300

    chore: create Details; add in main_graph.xml

---
+++ app/src/main/java/com/alvarengadev/alvaflix/view/details/DetailsFragment.kt
@@ -0,0 +1,22 @@
+package com.alvarengadev.alvaflix.view.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.alvarengadev.alvaflix.R
+
+class DetailsFragment : Fragment() {
+
+    private val viewModel: DetailsViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/alvarengadev/alvaflix/view/details/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.alvarengadev.alvaflix.view.details
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
