96f5a62937b118bc7c77a8ff0d044d74824a418b
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-09-22 10:33:32 -0300

    chore: create ApiDataSourceRepository.kt

---
+++ app/src/main/java/com/alvarengadev/alvaflix/data/repository/ApiDataSourceRepository.kt
@@ -0,0 +1,39 @@
+package com.alvarengadev.alvaflix.data.repository
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.alvarengadev.alvaflix.data.api.mapper.MoviePopularMapper
+import com.alvarengadev.alvaflix.data.api.network.RetrofitInitializer
+import com.alvarengadev.alvaflix.data.api.network.popular.reponse.MoviePopularResponseBody
+import com.alvarengadev.alvaflix.data.api.network.popular.reponse.MoviePopularResult
+import com.alvarengadev.alvaflix.data.domain.MoviePopular
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ApiDataSourceRepository(
+    private val liveData: MutableLiveData<ArrayList<MoviePopular>>
+) : Callback<MoviePopularResult> {
+
+    fun callRetrofitApi() =
+        RetrofitInitializer().movieService().list().enqueue(this)
+
+    override fun onResponse(
+        call: Call<MoviePopularResult>,
+        response: Response<MoviePopularResult>
+    ) {
+        if (response.isSuccessful) {
+            response.body()?.let { moviePopularResult ->
+                liveData.value = MoviePopularMapper.responseToDomain(moviePopularResult.results)
+            }
+        }
+    }
+
+    override fun onFailure(
+        call: Call<MoviePopularResult>,
+        t: Throwable
+    ) {
+        Log.i("AAAAAAAA", t.message.toString())
+    }
+
+}
