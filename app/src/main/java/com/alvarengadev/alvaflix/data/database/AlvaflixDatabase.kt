d1549984b056c900ccca3aaba760dacd7eb07af2
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-03-03 16:14:45 -0300

    chore: change text; add news strings; remove getInstance in AlvaflixDatabase.kt

--- app/src/main/java/com/alvarengadev/alvaflix/data/database/AlvaflixDatabase.kt
+++ app/src/main/java/com/alvarengadev/alvaflix/data/database/AlvaflixDatabase.kt
@@ -1,36 +1,11 @@
 package com.alvarengadev.alvaflix.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.alvarengadev.alvaflix.data.database.dao.MovieFavoritesDao
 import com.alvarengadev.alvaflix.data.database.entity.MovieEntity
-import com.alvarengadev.alvaflix.utils.Constants
 
 @Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
 abstract class AlvaflixDatabase : RoomDatabase() {
     abstract val movieFavoritesDao: MovieFavoritesDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AlvaflixDatabase? = null
-
-        fun getInstance(context: Context): AlvaflixDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AlvaflixDatabase::class.java,
-                    Constants.NAME_DATABASE
-                ).build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-
-    }
 }
