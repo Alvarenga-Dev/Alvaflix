bb044ebc985c0c4bfb6f129b4fbed1afdbc9cbdb
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-04-08 12:56:00 -0300

    test: remove unitTests.returnDefaultValues; change class test

--- app/src/test/java/com/alvarengadev/alvaflix/viewmodels/HomeViewModelUnitTest.kt
+++ app/src/test/java/com/alvarengadev/alvaflix/viewmodels/HomeViewModelUnitTest.kt
@@ -1,18 +1,21 @@
 package com.alvarengadev.alvaflix.viewmodels
 
 import com.alvarengadev.alvaflix.couroutine.MainCoroutineRule
-import com.alvarengadev.alvaflix.data.MoviesDataSourceFake
-import com.alvarengadev.alvaflix.data.api.network.MoviesApi
+import com.alvarengadev.alvaflix.data.api.network.popular.MoviePopularService
+import com.alvarengadev.alvaflix.data.api.network.popular.response.MoviePopularResult
+import com.alvarengadev.alvaflix.data.api.network.recommend.MovieRecommendService
+import com.alvarengadev.alvaflix.data.api.network.search.MovieSearchService
+import com.alvarengadev.alvaflix.data.api.network.similar.MovieSimilarService
 import com.alvarengadev.alvaflix.data.domain.Movie
 import com.alvarengadev.alvaflix.data.repository.api.MoviesApiRepositoryImpl
 import com.alvarengadev.alvaflix.view.home.HomeViewModel
 import io.mockk.*
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import java.util.*
 import kotlin.collections.ArrayList
 
 class HomeViewModelUnitTest {
@@ -21,10 +24,23 @@ class HomeViewModelUnitTest {
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    private val moviesApi = mockkClass(MoviesApi::class)
-    private val repository = MoviesApiRepositoryImpl(moviesApi)
-    private val listMockKMovies = mockk<ArrayList<Movie>>()
+    private val serviceMoviePopular = mockkClass(MoviePopularService::class)
+    private val serviceMovieRecommend = mockkClass(MovieRecommendService::class)
+    private val serviceMovieSimilar = mockkClass(MovieSimilarService::class)
+    private val serviceMovieSearch = mockkClass(MovieSearchService::class)
+    private val repository = MoviesApiRepositoryImpl(
+        serviceMoviePopular,
+        serviceMovieRecommend,
+        serviceMovieSimilar,
+        serviceMovieSearch
+    )
     private val homeViewModel = HomeViewModel(repository, mainCoroutineRule.dispatcher)
+
+    @MockK
+    private var listMockKMovies = ArrayList<Movie>()
+
+    @MockK
+    private var moviePopularResponseBody: MoviePopularResult? = null
 
     @Before
     fun before() {
@@ -36,22 +52,32 @@ class HomeViewModelUnitTest {
         val mockGetResponse = null
         coEvery { repository.getMoviesPopular() } returns mockGetResponse
         coVerify { repository.getMoviesPopular() }
-
-        coEvery { repository.getMoviesRecommend() } returns mockGetResponse
         coVerify { repository.getMoviesRecommend() }
 
-        assertNull(homeViewModel.listMoviePopularData.value)
-        assertNull(homeViewModel.listMovieRecommendData.value)
+        assertEquals(homeViewModel.listMoviePopularData.value, null)
+        assertEquals(homeViewModel.listMovieRecommendData.value, null)
     }
 
     @Test
     fun `when view model getListMoviePopular get success then sets listMoviePopularData`() {
-        val mockGetResponse = MoviesDataSourceFake().getListDataFakes(listMockKMovies)
-        coEvery { repository.getMoviesPopular() } returns mockGetResponse
+        listMockKMovies.add(
+            Movie(
+                10,
+                "Title 1",
+                "Poster Simple 1",
+                "Poster Details 1",
+                "Description 1",
+                "5.0",
+                "05/04/2021"
+            )
+        )
 
-        coEvery { repository.getMoviesRecommend() } returns mockGetResponse
+        coEvery { repository.getMoviesPopular() } returns listMockKMovies
+        coEvery { repository.getMoviesRecommend() } returns listMockKMovies
+        coVerify { repository.getMoviesPopular() }
+        coVerify { repository.getMoviesRecommend() }
 
-        assertNotNull(homeViewModel.listMoviePopularData.value)
-        assertNotNull(homeViewModel.listMovieRecommendData.value)
+        assertEquals(homeViewModel.listMoviePopularData.value, listMockKMovies)
+        assertEquals(homeViewModel.listMovieRecommendData.value, listMockKMovies)
     }
 }
